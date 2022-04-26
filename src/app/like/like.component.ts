06f543c053d4507303dd933fe8fe1818195f7f0a
Kiu Kyu <minkiukiu@gmail.com>
2022-04-26 17:19:15 +0700

    1st

---
+++ src/app/like/like.component.ts
@@ -0,0 +1,21 @@
+/* tslint:disable:no-trailing-whitespace */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+  like = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  likeThis() {
+    this.like++;
+  }
+}
